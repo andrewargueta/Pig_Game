6173a69ba48863223ea5ca6a22ffaed1176cdbc7
andrewargueta <49961926+andrewargueta@users.noreply.github.com>
2019-06-01 00:18:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -132,12 +132,4 @@
             }
         }
     });
-
-    /*
-
-    Change the game to follow these rules:
-    1. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-
-    */
-
 }());
